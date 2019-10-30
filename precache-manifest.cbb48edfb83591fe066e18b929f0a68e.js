7e519a63f3886a797d52d8c1bffa670a0cff9ad5
Alex <anebotov+skillful-alex@gmail.com>
2019-10-31 01:14:11 +0300

    first version

---
+++ precache-manifest.cbb48edfb83591fe066e18b929f0a68e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ef9e90433415ebfad68df7f0d0eddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4686beefec1416b6b67d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6b67c66a991701c6667f",
+    "url": "/static/js/2.8d2b119a.chunk.js"
+  },
+  {
+    "revision": "4686beefec1416b6b67d",
+    "url": "/static/js/main.8ff6f934.chunk.js"
+  },
+  {
+    "revision": "5167544ac678ebce9499",
+    "url": "/static/js/runtime-main.a61a2217.js"
+  }
+]);
